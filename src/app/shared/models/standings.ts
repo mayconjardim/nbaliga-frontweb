555b1de08ba519ab40131bce66f4447db78f44aa
Maycon Jardim <mayconsmill@gmail.com>
2022-12-04 01:14:18 -0300

    standings model

---
+++ src/app/shared/models/standings.ts
@@ -0,0 +1,25 @@
+export interface Standings {
+  id?: number;
+  name: string;
+  city: string;
+  streak: number;
+  homeWins: number;
+  homeLosses: number;
+  roadWins: number;
+  roadLosses: number;
+  win_: number;
+  points: number;
+  allowedPoints: number;
+  gb: number;
+  confGb: number;
+  divRank: number;
+  confRank: number;
+  conference: string;
+  totalWins: number;
+  totalLosses: number;
+  strk: string;
+  pa: number;
+  pf: number;
+  teamName: string;
+  diff: number;
+}
