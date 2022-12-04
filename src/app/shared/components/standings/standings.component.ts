5ad7bc8d8cde14a39281dca41886d0e0342e6128
Maycon Jardim <mayconsmill@gmail.com>
2022-12-03 22:04:46 -0300

    Componente standings

---
+++ src/app/shared/components/standings/standings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-standings',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './standings.component.html',
+  styleUrls: ['./standings.component.scss']
+})
+export class StandingsComponent {
+
+}
