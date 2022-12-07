907c2cf96f9086ffc6f3336f914b7568e5f2bd21
Maycon Jardim <mayconsmill@gmail.com>
2022-12-06 21:37:40 -0300

    Result component e service

--- src/app/shared/models/results.ts
+++ src/app/shared/models/results.ts
@@ -1,4 +1,4 @@
-export interface Schedule {
+export interface Results {
   content: [
     {
       day: number;
---
+++ src/app/shared/services/results.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of, tap } from 'rxjs';
+import { API_CONFIG } from 'src/app/config/api.config';
+
+import { Results } from '../models/results';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResultsService {
+  private results: Results[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  read() {
+    if (this.results.length) {
+      return of(this.results);
+    }
+
+    return this.http
+      .get<Results[]>(`${API_CONFIG.baseUrl}/schedule/results?size=10`)
+      .pipe(
+        tap((results) => {
+          this.results = results;
+        })
+      );
+  }
+}
