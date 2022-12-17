a65a32aaf28d5df647442e71eef7af81468d2fae
Maycon Jardim <mayconsmill@gmail.com>
2022-12-17 19:06:07 -0300

    Team roster component

---
+++ src/app/public/team/components/team-roster/team-roster.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'team-roster',
+  templateUrl: './team-roster.component.html',
+  styleUrls: ['./team-roster.component.scss'],
+})
+export class TeamRosterComponent {}
--- src/app/public/team/team.module.ts
+++ src/app/public/team/team.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TeamComponent } from './containers/team/team.component';
 import { TeamHeaderComponent } from './components/team-header/team-header.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TeamRosterComponent } from './components/team-roster/team-roster.component';
 
 export const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
-  declarations: [TeamComponent, TeamHeaderComponent],
+  declarations: [TeamComponent, TeamHeaderComponent, TeamRosterComponent],
   imports: [CommonModule, RouterModule.forChild(routes), MatTabsModule],
 })
 export class TeamModule {}
