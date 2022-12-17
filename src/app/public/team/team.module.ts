763dd37012d25b8aa3973428dbc25103e96f4303
Maycon Jardim <mayconsmill@gmail.com>
2022-12-17 19:02:32 -0300

    Team head card html e css, customizando tema do material

--- src/app/public/team/team.module.ts
+++ src/app/public/team/team.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { TeamComponent } from './containers/team/team.component';
 import { TeamHeaderComponent } from './components/team-header/team-header.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 export const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
   declarations: [TeamComponent, TeamHeaderComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatTabsModule],
 })
 export class TeamModule {}
