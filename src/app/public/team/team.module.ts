f2e1b77bd4bf236309dd6228643ce321ad56285e
Maycon Jardim <mayconsmill@gmail.com>
2022-12-17 21:43:08 -0300

    Player images, team roster card html e css

--- src/app/public/team/team.module.ts
+++ src/app/public/team/team.module.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
 
-import { TeamComponent } from './containers/team/team.component';
 import { TeamHeaderComponent } from './components/team-header/team-header.component';
-import { MatTabsModule } from '@angular/material/tabs';
 import { TeamRosterComponent } from './components/team-roster/team-roster.component';
+import { TeamComponent } from './containers/team/team.component';
 
 export const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
   declarations: [TeamComponent, TeamHeaderComponent, TeamRosterComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), MatTabsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatTabsModule,
+    MatIconModule,
+  ],
 })
 export class TeamModule {}
