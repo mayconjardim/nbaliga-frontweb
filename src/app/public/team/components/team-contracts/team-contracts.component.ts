827616a795705d1f6291dd393790e02f57440910
Maycon Jardim <mayconsmill@gmail.com>
2022-12-25 10:08:11 -0300

    team contracts html e css

--- src/app/public/team/components/team-contracts/team-contracts.component.ts
+++ src/app/public/team/components/team-contracts/team-contracts.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+
 import { Team } from '../../models/team';
+import { Player } from 'src/app/public/player/models/player';
 
 @Component({
   selector: 'team-contracts',
@@ -8,4 +10,82 @@ import { Team } from '../../models/team';
 })
 export class TeamContractsComponent {
   @Input() team!: Team;
+  player!: Player[];
+  capSpace: number = 101962352;
+
+  playerImg = 'assets/images/players/';
+  png = '.png';
+
+  ratingColors(rating: string) {
+    switch (rating) {
+      case 'A+':
+        return 'assets/images/utils/aa.png';
+      case 'A':
+        return 'assets/images/utils/a.png';
+      case 'B':
+        return 'assets/images/utils/b.png';
+      case 'C':
+        return 'assets/images/utils/c.png';
+      case 'D':
+        return 'assets/images/utils/d.png';
+      default:
+        return 'assets/images/utils/f.png';
+    }
+  }
+
+  getPosition(position: number) {
+    switch (position) {
+      case 5:
+        return 'PG';
+      case 4:
+        return 'SG';
+      case 3:
+        return 'SF';
+      case 2:
+        return 'PF';
+      default:
+        return 'C';
+    }
+  }
+
+  getTotalSalary(contract: number) {
+    let total;
+
+    switch (contract) {
+      case 1:
+        total = this.team.players?.reduce((acc, cur) => acc + cur.contract1, 0);
+        break;
+      case 2:
+        total = this.team.players?.reduce((acc, cur) => acc + cur.contract2, 0);
+        break;
+      case 3:
+        total = this.team.players?.reduce((acc, cur) => acc + cur.contract3, 0);
+        break;
+      case 4:
+        total = this.team.players?.reduce((acc, cur) => acc + cur.contract4, 0);
+        break;
+      case 5:
+        total = this.team.players?.reduce((acc, cur) => acc + cur.contract5, 0);
+        break;
+      default:
+        total = 0;
+        break;
+    }
+
+    return total;
+  }
+
+  getCapSpace(total: any) {
+    if (total > this.capSpace) {
+      return this.capSpace - total;
+    }
+    return total;
+  }
+
+  teamColors(team: string) {
+    if (team == '76ers') {
+      return 'Sixers';
+    }
+    return team;
+  }
 }
--- src/app/public/team/team.module.ts
+++ src/app/public/team/team.module.ts
@@ -4,15 +4,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
 
+import { TeamContractsComponent } from './components/team-contracts/team-contracts.component';
 import { TeamHeaderComponent } from './components/team-header/team-header.component';
 import { TeamRosterComponent } from './components/team-roster/team-roster.component';
 import { TeamComponent } from './containers/team/team.component';
-import { TeamContractsComponent } from './components/team-contracts/team-contracts.component';
 
 export const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
-  declarations: [TeamComponent, TeamHeaderComponent, TeamRosterComponent, TeamContractsComponent],
+  declarations: [
+    TeamComponent,
+    TeamHeaderComponent,
+    TeamRosterComponent,
+    TeamContractsComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
