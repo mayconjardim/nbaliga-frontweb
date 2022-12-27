283e962dd31f38ede1cd8fa2a3e590adc1b3526d
Maycon Jardim <mayconsmill@gmail.com>
2022-12-27 20:27:05 -0300

    Player rating html e css

--- src/app/public/player/player.module.ts
+++ src/app/public/player/player.module.ts
@@ -5,11 +5,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayerHeaderComponent } from './components/player-header/player-header.component';
 import { PlayerComponent } from './containers/player/player.component';
 import { PlayerRatingsComponent } from './components/player-ratings/player-ratings.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 export const routes: Routes = [{ path: '', component: PlayerComponent }];
 
 @NgModule({
-  declarations: [PlayerComponent, PlayerHeaderComponent, PlayerRatingsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    PlayerComponent,
+    PlayerHeaderComponent,
+    PlayerRatingsComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes), MatProgressBarModule],
 })
 export class PlayerModule {}
