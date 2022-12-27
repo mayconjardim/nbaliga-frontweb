46812aedb1b709f5fe00e096fd14ba50abc981c6
Maycon Jardim <mayconsmill@gmail.com>
2022-12-27 14:46:45 -0300

    Team schedule HTML e CSS

--- src/app/public/team/components/team-schedule/team-schedule.component.ts
+++ src/app/public/team/components/team-schedule/team-schedule.component.ts
@@ -13,4 +13,31 @@ export class TeamScheduleComponent {
 
   teamLogo = 'assets/images/logos/';
   svg = '.svg';
+
+  getGameType(day: number) {
+    if (day >= 1 && day <= 14) {
+      return 'PRESEASON';
+    } else if (day >= 15 && day <= 186) {
+      return 'REGULAR SEASON';
+    } else {
+      return 'PLAYOFFS';
+    }
+  }
+
+  getWinner(away: string, home: string, awayScore: number, homeScore: number) {
+    if (this.team.name == away) {
+      if (awayScore > homeScore) {
+        return 'W ';
+      } else {
+        return 'L ';
+      }
+    } else if (this.team.name == home) {
+      if (awayScore > homeScore) {
+        return 'L ';
+      } else {
+        return 'W ';
+      }
+    }
+    return '-';
+  }
 }
