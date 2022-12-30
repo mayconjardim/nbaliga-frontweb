4ed6a60fb77b2c9ab737e8232e6ba60f33937d8b
Maycon Jardim <mayconsmill@gmail.com>
2022-12-30 17:14:34 -0300

    Statistics container

--- src/app/public/statistics/statistics.module.ts
+++ src/app/public/statistics/statistics.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { StatisticsComponent } from './container/statistics/statistics.component';
 
-export const routes: Routes = [{ path: '' }];
+export const routes: Routes = [{ path: '', component: StatisticsComponent }];
 
 @NgModule({
-  declarations: [],
+  declarations: [StatisticsComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class StatisticsModule {}
