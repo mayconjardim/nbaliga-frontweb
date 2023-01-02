76b34cffe2a42f3d128b9d24a5a682ad42755727
Maycon Jardim <mayconsmill@gmail.com>
2023-01-02 15:32:58 -0300

    Draft class container

--- src/app/public/offseason/offseason.module.ts
+++ src/app/public/offseason/offseason.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DraftClassComponent } from './container/draft-class/draft-class.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+export const routes: Routes = [{ path: '', component: DraftClassComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DraftClassComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class OffseasonModule { }
+export class OffseasonModule {}
