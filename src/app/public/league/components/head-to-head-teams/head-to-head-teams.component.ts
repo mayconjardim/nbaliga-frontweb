64083a226306f11b93336a5349755b1cdc7bf48e
Maycon Jardim <mayconsmill@gmail.com>
2023-01-03 14:18:30 -0300

    Head-to-head-teams html e css

--- src/app/public/league/components/head-to-head-teams/head-to-head-teams.component.ts
+++ src/app/public/league/components/head-to-head-teams/head-to-head-teams.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Team } from 'src/app/public/team/models/team';
 
 @Component({
-  selector: 'app-head-to-head-teams',
+  selector: 'head-to-head-teams',
   templateUrl: './head-to-head-teams.component.html',
-  styleUrls: ['./head-to-head-teams.component.scss']
+  styleUrls: ['./head-to-head-teams.component.scss'],
 })
 export class HeadToHeadTeamsComponent {
-
+  @Input() teams!: Team[];
+  teamLogo = 'assets/images/logos/';
+  svg = '.svg';
 }
--- src/app/public/league/league.module.ts
+++ src/app/public/league/league.module.ts
@@ -4,12 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderModule } from 'ngx-order-pipe';
 
-import { SalaryCapTableComponent } from './components/salary-cap-table/salary-cap-table.component';
-import { SalaryCapComponent } from './containers/salary-cap/salary-cap.component';
-import { DraftPicksComponent } from './containers/draft-picks/draft-picks.component';
 import { DraftPicksTableComponent } from './components/draft-picks-table/draft-picks-table.component';
-import { HeadToHeadComponent } from './containers/head-to-head/head-to-head.component';
 import { HeadToHeadTeamsComponent } from './components/head-to-head-teams/head-to-head-teams.component';
+import { SalaryCapTableComponent } from './components/salary-cap-table/salary-cap-table.component';
+import { DraftPicksComponent } from './containers/draft-picks/draft-picks.component';
+import { HeadToHeadComponent } from './containers/head-to-head/head-to-head.component';
+import { SalaryCapComponent } from './containers/salary-cap/salary-cap.component';
 
 export const routes: Routes = [
   { path: 'salarycap', component: SalaryCapComponent },
